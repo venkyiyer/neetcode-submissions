282b10f770e971be99ba7c175f31781b8b4393d1
iVenky <33878259+venkyiyer@users.noreply.github.com>
2026-08-01 00:02:03 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        l = len(nums)/2
+        d = {}
+        for i in nums:
+            if i in d:
+                d[i] +=1
+            else:
+                d[i]=1
+        
+        if max(d.values()) > l:
+            return max(d, key=d.get)
+        
